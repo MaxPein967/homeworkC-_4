c446e763d81b3a9e44f847ea4bccde2ce0331525
Sergei Vasiliev <9670105747@mail.ru>
2023-02-10 11:16:27 +0700

    Add #27 sum numbers number

--- C#_27/Program.cs
+++ C#_27/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, 
+//которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(num);
+int sum = 0;
+
+while (num > 0)
+{
+int x = num % 10;
+num = num / 10;
+sum = sum + x;
+}
+Console.Write($" -> " + sum);
